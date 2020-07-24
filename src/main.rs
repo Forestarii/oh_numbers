54521f086b1a612c4e9479f4756fb5e298eb5df9
Forestarii <_Forestarii@users.noreply.github.com>
2020-07-24 20:41:01 +0300

    add loop cycle

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,25 @@ fn main()
     let random_number = rand::thread_rng().gen_range(1, 101);
     //println!("The secret number: {}", random_number);
 
-    println!("Please, input your guess.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess:i32 = guess.trim().parse().expect("Type a number.");
-
-    println!("Your guess: {}", guess);
-
-    match guess.cmp(&random_number)
+    loop
     {
-        Ordering::Less => println!("Too small..."),
-        Ordering::Equal => println!("You have won!"),
-        Ordering::Greater => println!("Too big!"),
+        println!("Please, input your guess number.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess:i32 = guess.trim().parse().expect("Type a number.");
+
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&random_number)
+        {
+            Ordering::Less => println!("Too small..."),
+            Ordering::Equal =>
+                {
+                    println!("You have won!");
+                    break;
+                },
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
 
